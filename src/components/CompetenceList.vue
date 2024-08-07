a6938a0ff6e03689b1c9daa25163c575de3471c6
ankur1812 <ankur.anmol1992@gmail.com>
2024-08-07 19:58:09 +0200

    add list/grid views + radio button

--- src/components/CompetenceList.vue
+++ src/components/CompetenceList.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="competences flex flex-col gap-4">
     <h1>Competences</h1>
+    <div class="radio-container">
+      <RadioButton v-model="currentView" :options="availableViews" />
+    </div>
     <div
       class="flex flex-wrap gap-4"
       :class="{
-        'flex-col': true
+        'flex-col': currentView == 'list',
+        'flex-row': currentView !== 'list'
       }"
     >
       <div
         v-for="skill in skills"
         :key="skill.id"
         :class="{
-          'w-full': true
+          'w-full': currentView == 'list',
+          'w-small': currentView == 'grid'
         }"
       >
         <competence-item :skill="skill" :view="currentView" />
@@ -22,14 +27,42 @@
 
 <script>
 import CompetenceItem from "./CompetenceItem";
+import RadioButton from "./RadioButton";
 
 export default {
   name: "CompetenceList",
   components: {
-    CompetenceItem
+    CompetenceItem,
+    RadioButton
   },
   props: {
     skills: { type: Array, default: () => [] }
+  },
+  data() {
+    return {
+      currentView: "list",
+      availableViews: [
+        { name: "List", value: "list" },
+        { name: "Grid", value: "grid" }
+      ]
+    };
   }
 };
 </script>
+<style scoped>
+@media screen and (max-width: 767px) {
+  .w-small {
+    width: 100%;
+  }
+}
+@media screen and (min-width: 768px) and (max-width: 1023px) {
+  .w-small {
+    width: 45%;
+  }
+}
+@media screen and (min-width: 1024px) {
+  .w-small {
+    width: 30%;
+  }
+}
+</style>
---
+++ src/components/RadioButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="radio-container flex">
+    <div v-for="(opt, index) in options" :key="opt.label">
+      <label
+        class="cursor-pointer py-1 px-2 border border-gray-400"
+        :class="{
+          'border-r-0': index == options.lenght - 1,
+          'bg-blue-400 text-white': modelValue === opt.value
+        }"
+      >
+        <input
+          type="radio"
+          class="hidden"
+          :value="opt.value"
+          :checked="modelValue === opt.value"
+          @change="updateValue(opt.value)"
+        />
+        {{ opt.name }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RadioButton",
+  props: {
+    modelValue: {
+      type: String,
+      default: "list"
+    },
+    options: {
+      type: Array,
+      default: () => []
+    }
+  },
+  emits: ["update:modelValue"],
+  methods: {
+    updateValue(value) {
+      this.$emit("update:modelValue", value);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
