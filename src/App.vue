4a9a47d7843e20223c9b9aa925319a50a304069c
ankur1812 <ankur.anmol1992@gmail.com>
2024-08-08 15:52:51 +0200

    add procademy icons

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,13 @@ export default {
 <style>
 @import "https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css";
 @import "https://cdn.syncfusion.com/ej2/material.css";
+@import "./assets/styles/styles.min.css";
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  margin-top: 100px;
 }
 </style>
