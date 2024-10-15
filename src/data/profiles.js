d994995bc55168214c8e631c1b847d7aba79be8e
ankur1812 <ankur.anmol1992@gmail.com>
2024-10-15 14:06:37 +0200

    add profiles data

---
+++ src/data/profiles.js
@@ -0,0 +1,114 @@
+export const profiles = {
+    "result": [
+        {
+            "id": 1,
+            "title": "Front-end developer",
+            "achieved": true
+        },
+        {
+            "id": 2,
+            "title": "Back-end developer",
+            "achieved": false
+        },
+        {
+            "id": 3,
+            "title": "QA-egineer",
+            "achieved": true
+        }
+    ],
+    "count": 3
+}
+
+export const p1 = {
+  "id": 1,
+  "title": "Front-end developer",
+  "description": "Front-end web development is the development of the graphical user interface of a website through the use of HTML, CSS, and JavaScript so users can view and interact with that website.",
+  "achieved": true,
+  "competences": [
+    {
+      "id": 1,
+      "title": "HTML",
+      "required": 100,
+      "achieved": 100
+    },
+    {
+      "id": 2,
+      "title": "CSS",
+      "required": 4,
+      "achieved": 4
+    },
+    {
+      "id": 3,
+      "title": "JavaScript",
+      "required": 200,
+      "achieved": 200
+    }
+  ]
+}
+
+export const p2 = {
+  "id": 2,
+  "title": "Back-end developer",
+  "description": "A backend developer could just be focused on a database and providing an API to it or on a server language like PHP and those mentioned so far and provide an API to it. Their work would need to be accessible to many other back end applications such as the database developer example and more to many front end applications. For example today many web backends service not only desktop and mobile web front ends but also device apps and even desktop apps. So the back end developer will not know how all of those front ends work but provide a server side interface such as a REST interface for those multiple front end technologies to access such as a login. Server developer can also make user interfaces that are probably less robust for administrative access to their code, functionality and data.",
+  "achieved": false,
+  "competences": [
+    {
+      "id": 1,
+      "title": "PHP",
+      "required": 100,
+      "achieved": 50
+    },
+    {
+      "id": 2,
+      "title": "SQL",
+      "required": 50,
+      "achieved": 50
+    },
+    {
+      "id": 3,
+      "title": "Laravel",
+      "required": 20,
+      "achieved": 14
+    },
+    {
+      "id": 3,
+      "title": "Object oriented programming",
+      "required": 80,
+      "achieved": 20
+    }
+  ]
+}
+
+export const p3 = {
+  "id": 3,
+  "title": "QA-egineer",
+  "description": "Quality Assurance (QA) is a way of preventing mistakes and defects in manufactured products and avoiding problems when delivering products or services to customers.",
+  "achieved": true,
+  "competences": [
+    {
+      "id": 1,
+      "title": "HTML",
+      "required": 50,
+      "achieved": 50
+    },
+    {
+      "id": 2,
+      "title": "CSS",
+      "required": 4,
+      "achieved": 4
+    },
+    {
+      "id": 3,
+      "title": "JavaScript",
+      "required": 120,
+      "achieved": 120
+    },
+    {
+      "id": 3,
+      "title": "SQL",
+      "required": 20,
+      "achieved": 20
+    }
+  ]
+}
+
